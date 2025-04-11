352662fae35375b0b60f719f3fa8819a2b026a7d
MariamEssam316 <mariamessam753@gmail.com>
2025-04-11 06:11:28 +0200

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,21 @@
+<?php
+$x=10;
+$y=20;
+echo $x+$y;
+
+echo "<br>";
+
+
+$a=4;
+++$a;
+echo "I have ".$a++." books";
+echo "<br>";
+
+
+$sentence=null;
+$first="In";
+$second="PHP";
+$third="Course";
+echo $sentence=$first." ".$second." ".$third;
+
+?>
